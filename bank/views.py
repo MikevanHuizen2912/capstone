39c44305252f0754b535199af2aeabcb50412c9e
MikevanHuizen2912 <mike_van_huizen@hotmail.com>
2023-03-29 21:49:20 +0200

    Create a simple login form

--- bank/views.py
+++ bank/views.py
@@ -1,5 +1,29 @@
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return render(request, "bank/index.html")
+    if request.user.is_authenticated:
+        return render(request, "bank/index.html")
+    else:
+        return login(request)
+
+def login(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "bank/login.html", {
+                "message": "The password or email that was entered was incorrect!"
+            })
+
+    return render(request, "bank/login.html")
+
+def register(request):
+    return render(request, "bank/register.html")
