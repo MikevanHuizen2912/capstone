6d5eeee363a343972d0159f87578a018e7e0fbc0
MikevanHuizen2912 <mike_van_huizen@hotmail.com>
2023-03-30 11:42:06 +0200

    Changed model for transaction and migrated it

--- bank/models.py
+++ bank/models.py
@@ -14,6 +14,7 @@ class Bankaccount(models.Model):
     interest = models.IntegerField()
 
 class Transaction(models.Model):
+    name = models.CharField(max_length=20)
     sender = models.ForeignKey('Bankaccount', on_delete=models.CASCADE, related_name="send_transaction")
     receiver = models.ForeignKey('Bankaccount', on_delete=models.CASCADE, related_name="receive_transaction")
     date = models.DateTimeField()
