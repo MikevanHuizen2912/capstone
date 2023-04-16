e7bc87038cb4f4a71baeb89f113c5773c3b20edb
MikevanHuizen2912 <mike_van_huizen@hotmail.com>
2023-04-16 21:14:15 +0200

    Working further on the CSS animation

--- bank/static/bank/layout.js
+++ bank/static/bank/layout.js
@@ -1,15 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#mobile_header').style.display = 'hidden'
+    document.querySelector('#mobile_header').style.display = 'none'
     document.querySelector('.navbar-toggler').addEventListener('click', () => {
         if(document.querySelector('#mobile_header').style.display === 'none'){
             console.log('appear')
-            document.querySelector('#mobile_header').style.display = 'block'
-            document.querySelector('.appear').style.animationPlayState = 'running'
+            document.querySelector('#mobile_header').style.display = 'block';
+            document.querySelector('#appear').style.animationPlayState = 'running'
         }
         else if(document.querySelector('#mobile_header').style.display === 'block'){
             console.log('disappear')
-            document.querySelector('#mobile_header').style.display = 'hidden'
-            document.querySelector('.disappear').style.animationPlayState = 'running'
-        }
+            document.querySelector('#disappear').style.animationPlayState = 'running'
+            document.querySelector('#disappear').addEventListener('animationend', () => {
+                document.querySelector('#mobile_header').style.display = 'none';
+            });        }
     })
 })
