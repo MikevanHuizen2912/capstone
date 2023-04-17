85dd086190bbb4e4acd8630a45fc4da2604efc70
MikevanHuizen2912 <mike_van_huizen@hotmail.com>
2023-04-17 20:28:47 +0200

    removed the animation

--- bank/static/bank/layout.js
+++ bank/static/bank/layout.js
@@ -3,11 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.navbar-toggler').addEventListener('click', () => {
         if(document.querySelector('#mobile_header').style.display === 'none'){
             document.querySelector('#mobile_header').style.display = 'block';
-            document.querySelector('#appear').style.animationPlayState = 'running';
         }
         else if(document.querySelector('#mobile_header').style.display === 'block'){
-            document.querySelector('#disappear').style.animationPlayState = 'running';
-            document.querySelector('#mobile_header').style.display = 'none'; 
+            document.querySelector('#mobile_header').style.display = 'none';
         }    
     })
 })
