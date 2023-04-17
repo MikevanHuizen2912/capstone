c553e5786f1acf4f6c5377f92599730c894eff56
MikevanHuizen2912 <mike_van_huizen@hotmail.com>
2023-04-17 16:55:36 +0200

    Trying to fix the animation

--- bank/static/bank/layout.js
+++ bank/static/bank/layout.js
@@ -2,15 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#mobile_header').style.display = 'none'
     document.querySelector('.navbar-toggler').addEventListener('click', () => {
         if(document.querySelector('#mobile_header').style.display === 'none'){
-            console.log('appear')
             document.querySelector('#mobile_header').style.display = 'block';
-            document.querySelector('#appear').style.animationPlayState = 'running'
+            document.querySelector('#appear').style.animationPlayState = 'running';
         }
         else if(document.querySelector('#mobile_header').style.display === 'block'){
-            console.log('disappear')
-            document.querySelector('#disappear').style.animationPlayState = 'running'
-            document.querySelector('#disappear').addEventListener('animationend', () => {
-                document.querySelector('#mobile_header').style.display = 'none';
-            });        }
+            document.querySelector('#disappear').style.animationPlayState = 'running';
+            document.querySelector('#mobile_header').style.display = 'none'; 
+        }    
     })
 })
